9a9a58db4c596e57bf932a2f348d5b787b262a6c
Norm <normandmickey@gmail.com>
2018-02-09 05:16:42 -0500

    chapter 8

--- truffle.js
+++ truffle.js
@@ -5,8 +5,9 @@ module.exports = {
   networks: {
     development: {
       host: '192.168.1.12',
-      port: 7545,
-      network_id: '*' // Match any network id
+      port: 8545,
+      network_id: '4', // Match any network id
+      from: "0x9758238291b3fadb9a9488698965d38b8f3bf4ac"
     }
   }
 }
